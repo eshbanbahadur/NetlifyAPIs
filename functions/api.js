50a24710db58681fc5524ddb4c1f55335650769a
KomalKath <149062619+KomalKath@users.noreply.github.com>
2024-08-15 14:49:35 +0500

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -3,9 +3,19 @@ const serverless = require('serverless-http');
 
 const app = express();
 
+const courseRoutes = require('../routes/courseRoutes');
+const instructorRoutes = require('../routes/instructorRoutes');
+const studentRoutes = require('../routes/studentRoutes');
+const takeRoutes = require('../routes/takeRoutes');
+
+app.use('/.netlify/functions/api/courses', courseRoutes);
+app.use('/.netlify/functions/api/instructors', instructorRoutes);
+app.use('/.netlify/functions/api/students', studentRoutes);
+app.use('/.netlify/functions/api/takes', takeRoutes);
+
 // Your existing Express routes
-app.get('/.netlify/functions/api/hello', (req, res) => {
-  res.json({ message: 'Hello World' });
-});
+// app.get('/.netlify/functions/api/hello', (req, res) => {
+//   res.json({ message: 'Hello World' });
+// });
 
 module.exports.handler = serverless(app);
