cf99368a5d143e0f11fba4b75cc7aa6d94f12a23
KomalKath <149062619+KomalKath@users.noreply.github.com>
2024-08-15 15:04:11 +0500

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -3,19 +3,19 @@ const serverless = require('serverless-http');
 
 const app = express();
 
-const courseRoutes = require('../routes/courseRoutes');
-const instructorRoutes = require('../routes/instructorRoutes');
-const studentRoutes = require('../routes/studentRoutes');
-const takeRoutes = require('../routes/takeRoutes');
+// const courseRoutes = require('../routes/courseRoutes');
+// const instructorRoutes = require('../routes/instructorRoutes');
+// const studentRoutes = require('../routes/studentRoutes');
+// const takeRoutes = require('../routes/takeRoutes');
 
-app.use('/.netlify/functions/api/course', courseRoutes);
-app.use('/.netlify/functions/api/instructor', instructorRoutes);
-app.use('/.netlify/functions/api/student', studentRoutes);
-app.use('/.netlify/functions/api/take', takeRoutes);
+// app.use('/.netlify/functions/api/course', courseRoutes);
+// app.use('/.netlify/functions/api/instructor', instructorRoutes);
+// app.use('/.netlify/functions/api/student', studentRoutes);
+// app.use('/.netlify/functions/api/take', takeRoutes);
 
-// Your existing Express routes
-// app.get('/.netlify/functions/api/hello', (req, res) => {
-//   res.json({ message: 'Hello World' });
-// });
+Your existing Express routes
+app.get('/.netlify/functions/api/course/all', (req, res) => {
+  res.json({ message: 'Hello World' });
+});
 
 module.exports.handler = serverless(app);
