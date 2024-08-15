75a659e1fc31ad4ffae875eb830cc2b7eae34057
KomalKath <149062619+KomalKath@users.noreply.github.com>
2024-08-15 14:54:28 +0500

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -8,10 +8,10 @@ const instructorRoutes = require('../routes/instructorRoutes');
 const studentRoutes = require('../routes/studentRoutes');
 const takeRoutes = require('../routes/takeRoutes');
 
-app.use('/.netlify/functions/api/courses', courseRoutes);
-app.use('/.netlify/functions/api/instructors', instructorRoutes);
-app.use('/.netlify/functions/api/students', studentRoutes);
-app.use('/.netlify/functions/api/takes', takeRoutes);
+app.use('/.netlify/functions/api/course', courseRoutes);
+app.use('/.netlify/functions/api/instructor', instructorRoutes);
+app.use('/.netlify/functions/api/student', studentRoutes);
+app.use('/.netlify/functions/api/take', takeRoutes);
 
 // Your existing Express routes
 // app.get('/.netlify/functions/api/hello', (req, res) => {
