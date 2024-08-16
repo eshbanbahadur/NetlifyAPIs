8ddd285d259240a0745b2a2b076bdf8ff2de3919
Eshban Bahadur <43173614+eshbanbahadur@users.noreply.github.com>
2024-08-16 10:55:51 +0500

    Update courseRoutes.js

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -3,12 +3,6 @@ const courseController = require('../controllers/courseController');
 const router = express.Router();
 
 // Define the routes
-router.get('/course/:courseId/student-ids', courseController.getStudentIdsFromCourse);
 router.get('/all', courseController.getAllCourse);
-router.get('/:id', courseController.getCourseById);
-router.get('/:id', courseController.getStudentbyListofCourse);
-router.post('/add', courseController.createCourse);
-router.delete('/:id', courseController.deleteCourse);
-router.put('/:id', courseController.updateCourse);
 
 module.exports = router;
