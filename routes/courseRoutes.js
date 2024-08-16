b7b84c9e455a85f2fb33b1372c0ea45d53128f56
Eshban Bahadur <43173614+eshbanbahadur@users.noreply.github.com>
2024-08-16 11:04:28 +0500

    Update courseRoutes.js

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -2,6 +2,12 @@ const express = require('express');
 const courseController = require('../controllers/courseController');
 const router = express.Router();
 
+
+// Simple test route within courseRoutes.js
+router.get('/test-route', (req, res) => {
+  res.json({ message: 'Course route working!' });
+});
+
 // Define the routes
 router.get('/all', courseController.getAllCourse);
 
