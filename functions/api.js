e999b94e742e0b232fb77a3eb8da27a36c6b1495
Eshban Bahadur <43173614+eshbanbahadur@users.noreply.github.com>
2024-08-16 11:05:26 +0500

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -6,4 +6,10 @@ const app = express();
 const courseRoutes = require('../routes/courseRoutes'); 
 app.use('/.netlify/functions/api/course', courseRoutes);
 
+// Basic test route to verify that `api.js` is working
+app.get('/.netlify/functions/api/test', (req, res) => {
+  res.json({ message: 'Test route working!' });
+});
+
+
 module.exports.handler = serverless(app);
