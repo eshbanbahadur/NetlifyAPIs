e4acd0092f3dd2fbe27072c1476b2f8d3f51b4a8
KomalKath <149062619+KomalKath@users.noreply.github.com>
2024-08-16 10:34:32 +0500

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -16,10 +16,10 @@ const app = express();
 //Your existing Express routes
 // app.get('/.netlify/functions/api/course/all', (req, res) => {
 //   res.json({ message: 'Hello World' });
-
+// });
   // Import routes from the root-level routes folder
 const courseRoutes = require('../routes/courseRoutes'); 
 app.use('/.netlify/functions/api/course', courseRoutes);
-});
+
 
 module.exports.handler = serverless(app);
